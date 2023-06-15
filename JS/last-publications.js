5b1c67f42dce48e29dd56de89eb479f741ac0fdb
Idir Ait-Sadoune <idir.aitsadoune@gmail.com>
2023-06-15 11:16:20 +0200

    refactoring the code of the last publications section

--- JS/last-publications.js
+++ JS/last-publications.js
@@ -6,33 +6,26 @@ function createElement(tagName, attributes = {}){
     return element
 }
 
-function getLastPublications(){
-    const main_div = document.getElementById("last-publications")
+const main_div = document.getElementById("last-publications")
+const headers = {Accept: 'application/json'}
 
-    const headers = {Accept: 'application/json'}
-    fetch("./JSON/last-pub.json", headers)
-        .then((response) => response.json())
-        .then((json) => {
-            
-            const publications = json["publications"]
-            for (let i=0; i<4 ; i++){
-                const div = createElement('div')
-                const a = createElement('a',{href : publications[i]["link"], target : "_blank", class : "publication-title"})
-                a.appendChild(createElement('i',{class : "fa fa-book"}))
-                a.appendChild(document.createTextNode(" " + publications[i]["title"]))
-                //console.log(publications[i]["title"])
-                div.appendChild(a)
-                main_div.appendChild(div)
-                main_div.appendChild(document.createTextNode(" " + publications[i]["conference"]))
+fetch("./JSON/last-pub.json", headers)
+    .then((response) => response.json())
+    .then((json) => {
+        const publications = json["publications"]
+
+        // Only the last four publications
+        for (let i=0; i<4 ; i++){
+            const div = createElement('div')
+            const a = createElement('a',{href : publications[i]["link"], target : "_blank", class : "publication-title"})
+            a.appendChild(createElement('i',{class : "fa fa-book"}))
+            a.appendChild(document.createTextNode(" " + publications[i]["title"]))
+            div.appendChild(a)
+            main_div.appendChild(div)
+            main_div.appendChild(document.createTextNode(" " + publications[i]["conference"]))
+            main_div.appendChild(document.createElement("br"))
+            if(i < 3) {
                 main_div.appendChild(document.createElement("br"))
-                if(i < 3) {
-                    main_div.appendChild(document.createElement("br"))
-                }
             }
-            
-    });
-
-    
-}
-
-getLastPublications()
+        }      
+});
