e26213bb2042524bf6cb146ff3bd5506ca8bcb50
Idir Ait-Sadoune <idir.aitsadoune@gmail.com>
2023-11-05 10:19:07 +0100

    Redirecting MathJax plugin to a local reference in RevealJs

--- enseignement/reveal-js-framework/plugin/math/mathjax3.js
+++ enseignement/reveal-js-framework/plugin/math/mathjax3.js
@@ -14,6 +14,7 @@ export const MathJax3 = () => {
         tex: {
             inlineMath: [ [ '$', '$' ], [ '\\(', '\\)' ]  ]
         },
+        loader: {load: ['ui/lazy']},
         options: {
             skipHtmlTags: [ 'script', 'noscript', 'style', 'textarea', 'pre' ]
         },
@@ -59,7 +60,7 @@ export const MathJax3 = () => {
             options.options = {...defaultOptions.options, ...revealOptions.options}
             options.startup = {...defaultOptions.startup, ...revealOptions.startup}
 
-            let url = options.mathjax || 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+            let url = options.mathjax || '../../enseignement/resources/MathJax/es5/tex-mml-chtml.js' // 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
             options.mathjax = null;
 
             window.MathJax = options;
